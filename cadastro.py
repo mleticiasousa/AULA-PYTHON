0a80f40e6632d5ea0b07019f39e0deb0ecb99fc6
mleticiasousa <letisousa009@gmail.com>
2023-07-18 19:50:26 -0300

    Subindo arquivo da aula 18/07

--- cadastro.py
+++ cadastro.py
@@ -1,24 +1,9 @@
 name = str(input("Digite seu nome: "))
-nome = str(input("Digite seu sobrenome: "))
+lastname = str(input("Digite seu sobrenome: "))
 age = int(input("Idade: "))
-nome = str(input("E-mail: "))
-nome = int(input("Telefone: "))
-nome = str(input("Deixe aqui sua sugestão: "))
+email = str(input("E-mail: "))
+fone = int(input("Telefone: "))
+suggest = str(input("Deixe aqui sua sugestão: "))
+nome_completo = name + " " + lastname
 
-def obter_nome():
-    nome = input("Digite seu nome: ")
-    return nome
-
-def feedback_personalizado(name):
-    return f"Olá, {name}! Obrigado por preencher o formulário."
-
-def main():
-    nome_usuario = obter_nome()
-    feedback = feedback_personalizado(nome_usuario)
-    print(feedback)
-
-if __name__ == "__main__":
-    main()
-
-print( )
-print("Cadastro realzizado com sucesso!")
+print("Cadastro realzizado com sucesso,", nome_completo+"!")
