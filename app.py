c5edda9267daad22cc05284908d99dd70bf305a7
mleticiasousa <letisousa009@gmail.com>
2023-07-27 20:18:17 -0300

    instalação do flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+#Objeto/servidor do flask
+app = Flask(__name__)
+
+#rota principal
+@app.route('/')
+def hello():
+     return "Olá Mundo!"
+ 
+ #execução do servidor
+if __name__ == "__main__":
+        app.run(debug=True)
